28f5b142acacaaca5c7b8c1753a3a7f3a5ba59bf
Tejansh Maurya <110341304+tejas-maurya@users.noreply.github.com>
2026-02-07 16:21:00 +0530

    Routes Setup Done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,23 @@
+import { Route,Routes } from 'react-router-dom';
 import './App.css';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+import DonorDashboard from './pages/DonorDashboard';
+import DoctorDashboard from './pages/DoctorDashboard';
+import LearnMore from "./pages/LearnMore";
 
 function App() {
   return (
-    <div className="App">
-      APP
+    <div >
+      <Routes>
+        <Route path='/' element={<Home/>}></Route>
+        <Route path='/donor-dashboard' element={<DonorDashboard/>}></Route>
+        <Route path='/doctor-dashboard' element={<DoctorDashboard/>}></Route>
+        <Route path='/login' element={<Login/>}></Route>
+        <Route path='/signup' element={<Signup/>}></Route>
+        <Route path="/learn-more" element={<LearnMore />} />
+      </Routes>
     </div>
   );
 }
