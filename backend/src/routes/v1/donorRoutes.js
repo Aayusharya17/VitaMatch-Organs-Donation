0cb091f34d108c8a94a271b405b024e14d8e5bca
dhiman31 <ankushiiituna@gmail.com>
2026-02-07 17:19:43 +0530

    Wrote routes names only

---
+++ backend/src/routes/v1/donorRoutes.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+const donorController = require('../../controllers/donorController');
+const {authMiddleware} = require('../../middleware/auth');
+
+router.post('/donateOrgan',authMiddleware,donorController.createDonation); // working fine
+router.post('/confirmDonation',authMiddleware,donorController.confirmDonation); // working fine
+router.get('/waitingOrgans',authMiddleware,donorController.findAllRequests); // working fine
+router.post("/confirm-allocation/:id", authMiddleware, donorController.confirmAllocation); // working fine
+router.post("/reject-allocation/:id", authMiddleware, donorController.rejectAllocation);
+router.get('/all', authMiddleware, donorController.findAll);
+
+module.exports = router;
